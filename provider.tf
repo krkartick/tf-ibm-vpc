49dcc03d0208a79bc887803912982f050e6584b5
krkartick <krkartick@gmail.com>
2020-07-21 00:01:12 -0700

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,4 +1,11 @@
 # Configure the IBM Cloud Provider
+//--------------------------------------------------------------------
+// Modules
+module "ibm" {
+  source  = "app.terraform.io/KartickOrg/ibm/provider"
+  version = "0.30.1"
+}
+  
 provider "ibm" {
   ibmcloud_api_key   = var.ibmcloud_api_key
   generation         = 1
